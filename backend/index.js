94ace4a8b1ed9347be24f3c031c3692b76645106
vivek <vivekvikram777@gmail.com>
2023-08-25 02:06:42 +0530

    mongo-connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const connectDB = async () => {
+  mongoose.connect("mongodb://localhost:27017/e-comm");
+  const productSchema = new mongoose.Schema({});
+  const product = mongoose.model("products", productSchema);
+  const data = await product.find();
+  console.log(data);
+};
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("app is working....");
